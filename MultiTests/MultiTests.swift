03e83172a943c4eb14852d1240ba7f7461694d1b
Alex Kern <alex@kern.io>
2018-06-06 17:11:35 -0700

    Initial Commit

---
+++ MultiTests/MultiTests.swift
@@ -0,0 +1,36 @@
+//
+//  MultiTests.swift
+//  MultiTests
+//
+//  Created by Alexander Kern on 6/6/18.
+//  Copyright © 2018 Distributed Systems, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Multi
+
+class MultiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
